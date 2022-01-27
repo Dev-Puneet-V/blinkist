bb8db62649ba377c14aa263ee625a2d7b11d1717
puneet-iner <imhtcy312@gmail.com>
2022-01-27 20:39:57 +0530

    card snapshot

---
+++ src/component/organism/Card/index.test.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import BookCard from ".";
+import { ThemeProvider } from "@material-ui/core/styles";
+import renderer from 'react-test-renderer';
+import {BrowserRouter} from 'react-router-dom'
+const props = {
+    id: 0,
+    imgHeight: 300,
+    url: '/assets/book.png',
+    bookName: "Being Boss",
+    writerName: "Kathleen Shannon and Emily...",
+    timeRead: "13-minute read",
+    width: 350,
+    inLibrary: false,
+};
+
+test('BookCard', () => {
+    const component = renderer.create(
+        <BrowserRouter>
+            <BookCard {...props}></BookCard>
+        </BrowserRouter>
+    );
+    expect(component).toMatchSnapshot();
+});
